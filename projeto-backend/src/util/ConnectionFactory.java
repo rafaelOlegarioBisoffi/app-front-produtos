e46cf68f6f584b7ecdf2bf2efe16bc06ab5c8c30
rafaelOlegarioBisoffi <rafaelolegariobisoffi@gmail.com>
2025-11-24 19:55:22 -0300

    erro banco de dados fix

--- projeto-backend/src/util/ConnectionFactory.java
+++ projeto-backend/src/util/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/app-produtos";
+    private static final String URL = "jdbc:mysql://localhost:3306/app_produtos";
     private static final String USER = "root";
     private static final String PASS = "aluno";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
