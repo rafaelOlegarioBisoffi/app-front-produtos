9ea06fcd0d8c63ade2f11cb54c38e8e9f7249dca
rafaelOlegarioBisoffi <rafaelolegariobisoffi@gmail.com>
2025-11-24 19:57:40 -0300

    configura cors

--- projeto-backend/src/App.java
+++ projeto-backend/src/App.java
@@ -9,6 +9,27 @@ public class App {
     public static void main(String[] args) {
         port(8080);
 
+          options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "http://localhost:3000");
+            response.header("Access-Control-Allow-Credentials", "true");
+            response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        });
+
         ApiCategoria.Execute();
         ApiProduto.Execute();
 
